622d17d5cd55e19b14b2035c321c00135e48b6ee
Daniel Zaltsman <daniel.zaltsman92@gmail.com>
2019-07-31 17:22:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,30 +19,15 @@ function inc(n) {
   return n++
 }
 
-it('dec(n) decrements n and returns the result', function() {
-  expect(dec(a)).toEqual(a - 1)
-})
+function dec(n){
+  return n--
+  
+}
 
-describe('makeInt(n)', function() {
-  it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
+function makeInt(n){
+  return parseInt(n)
+}
 
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})
-
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
+  function preserveDecimal(n){
+    return parseFloat(n)
+  }
